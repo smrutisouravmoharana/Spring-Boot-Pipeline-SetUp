4c21847f294213a43bb749b752f11d2ed13bba15
smrutisouravmoharana <ssmoharana2023@gmail.com>
2025-08-31 10:17:19 +0530

    Initial commit

---
+++ src/main/java/com/sourav/springbootpipeline/SpringbootpipelineApplication.java
@@ -0,0 +1,13 @@
+package com.sourav.springbootpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootpipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sourav/springbootpipeline/SpringbootpipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourav.springbootpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootpipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
